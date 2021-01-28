741fbd421810db7d3c5b3b8d04eb8c214e90c589
rajesh durgam <drvgao@users.noreply.github.com>
2021-01-28 21:08:41 +0530

    Create InterviewQuestions.py
    Different ways to select a random element

---
+++ InterviewQuestions.py
@@ -0,0 +1,9 @@
+# how to generate random elements from list using random.choice method
+import random
+foo = [1,2,5,6,7]
+random_element = random.choice(foo)
+print(random_element)
+
+# with secrets module
+
+# with randrange
