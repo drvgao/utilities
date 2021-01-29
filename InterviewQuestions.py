6bb85cd05ca760568adefb539503e8fd1299e505
rajesh durgam <drvgao@users.noreply.github.com>
2021-01-29 16:26:46 +0530

    memory efficient tips
    Follow this, https://towardsdatascience.com/memory-management-in-python-6bea0c8aecc9#:~:text=The%20Python%20memory%20manager%20manages%20chunks%20of%20memory%20called%20%E2%80%9CBlocks,object%20of%20the%20same%20size.

--- InterviewQuestions.py
+++ InterviewQuestions.py
@@ -7,3 +7,16 @@ print('random_element', random_element)
 # with secrets module
 
 # with randrange
+
+
+# Don't do this msg=’hello’+mymsg+’world’. instead try below
+msg=’hello %s world’ % mymsg
+
+#Don't
+# mymsg=’line1\n’
+# mymsg+=’line2\n’
+
+mymsg=[‘line1’,’line2']
+‘\n’.join(mymsg)
+       
+# https://towardsdatascience.com/memory-management-in-python-6bea0c8aecc9#:~:text=The%20Python%20memory%20manager%20manages%20chunks%20of%20memory%20called%20%E2%80%9CBlocks,object%20of%20the%20same%20size.     
